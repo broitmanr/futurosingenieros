296089e1b78577e0171b29050b14200169dde5c6
Román Broitman <broitmanroman@gmail.com>
2024-08-11 00:31:16 -0300

    fix: agrego not null a persona_id en usuario

--- back/src/models/usuario.js
+++ back/src/models/usuario.js
@@ -23,7 +23,7 @@ const Usuario = sequelize.define('Usuario', {
   },
   persona_id: {
     type: DataTypes.INTEGER,
-    allowNull: false,
+    allowNull: true,
     unique: true,
     references: {
       model: Persona,

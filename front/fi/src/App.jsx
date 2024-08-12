e9d21d7b6637e6d17b378487d1da431819c0c54d
VriaH <vicky627243@gmail.com>
2024-08-12 00:37:45 -0300

    Add: se añade footer

--- front/fi/src/App.jsx
+++ front/fi/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Home from './Home';
 
 
@@ -12,6 +13,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />}/>
             </Routes>
+            <Footer />
         </BrowserRouter>
     </div>
   );

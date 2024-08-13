9300464060df1dd70c468128bd6cdc1d12b75853
VriaH <vicky627243@gmail.com>
2024-08-13 03:23:58 -0300

    Add: se añade archivo para simular catedras

---
+++ front/fi/src/data.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        id: '1',
+        catedra: 'Proyecto final',
+        comision: 'S51',
+    },
+    {
+        id: '2',
+        catedra: 'Diseño de sistemas',
+        comision: 'S32',
+    },
+    {
+        id: '3',
+        catedra: 'Análisis de sistemas',
+        comision: 'S24',
+    },
+];

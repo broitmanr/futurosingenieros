91de1a19307967a7494c84f69f97bce8fa6a1fa3
VriaH <vicky627243@gmail.com>
2024-08-13 03:25:37 -0300

    Add: se añade pantalla Catedras y su ruta

--- front/fi/src/App.jsx
+++ front/fi/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Subheader from './components/Subheader';
 import Footer from './components/Footer';
 import Home from './Home';
+import Catedras from './Catedras'
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
             <Subheader />
             <Routes>
                 <Route path="/" element={<Home />}/>
+                <Route path="/catedras" element={<Catedras />}/>
             </Routes>
             <Footer />
         </BrowserRouter>

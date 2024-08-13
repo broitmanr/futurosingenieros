cb1942b6d0c2648f2b9de1d4d67c17eca2b520d6
VriaH <vicky627243@gmail.com>
2024-08-13 04:24:02 -0300

    Change: se añade login de prueba

--- front/fi/src/Home.jsx
+++ front/fi/src/Home.jsx
@@ -5,28 +5,56 @@ function Home(){
     return(
         <div className="home-container d-flex flex-column align-items-center" 
             style={{
-                //display: 'flex',
-                //flexDirection: 'column',
                 minHeight: '100vh', 
-                justifyContent: 'center'
+                marginTop: '10rem'
+                //justifyContent: 'center'
             }}>
             <h5 style={{ 
                 color: '#1A2035', 
                 fontWeight: 'bold', 
-                fontSize: '1.8rem'
-                }}>¡Bienvenidos a Futuros Ingenieros!</h5>    
-            <Link to="/catedras" style={{ textDecoration: 'none' }}>
-                <button className="btn" 
-                    style={{ 
-                        backgroundColor: '#1A2035', 
-                        color: '#fff', 
-                        marginTop: '2rem',
-                        width: '10rem',
-                        fontSize: '1.4rem'
-                    }}>
-                    Empezar
-                </button>
-            </Link>  
+                fontSize: '2.4rem', 
+                marginBottom: '12rem'
+                }}>¡Bienvenidos a Futuros Ingenieros!</h5>  
+            <form style={{ width: '40rem' }}> 
+
+                {/*Correo electrónico*/}
+
+                <div class="form-group" style={{ marginBottom: '3rem' }}>
+                    <label for="validationDefault01" style={{ fontSize: '1.4rem' }}>Correo electrónico</label>
+                    <input type="text" class="form-control" id="validationDefault01" placeholder="Inserte su correo electrónico" 
+                        style={{
+                            height: '3.6rem',
+                            border: '0.2rem solid #1A2035', 
+                            marginTop: '0.4rem',
+                            fontSize: '1.2rem'
+                        }} required />
+                </div>
+
+                {/*Contraseña*/}
+
+                <div class="form-group" style={{ marginBottom: '3rem' }}>
+                    <label for="validationDefault03" style={{ fontSize: '1.4rem' }}>Contraseña</label>
+                    <input type="text" class="form-control" id="validationDefault03" placeholder="Ingrese su contraseña" 
+                        style={{
+                            height: '3.6rem',
+                            border: '0.2rem solid #1A2035',
+                            marginTop: '0.4rem',
+                            fontSize: '1.2rem'
+                        }} required />
+                </div>
+                <Link to="/catedras" style={{ textDecoration: 'none' }}>
+                    <button className="btn btn-primary" /*type="submit"*/ 
+                        style={{ 
+                            backgroundColor: '#1A2035', 
+                            color: '#fff', 
+                            marginTop: '2rem',
+                            width: '10rem',
+                            fontSize: '1.4rem'
+                        }}>
+                        Empezar
+                    </button>
+                </Link>  
+            </form> 
         </div>
     )
 }

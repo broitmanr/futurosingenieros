0c2de860b57e7959dbd066d842642ef33b5decf2
FrancoTastaca <franc0tastac@gmail.com>
2024-08-22 12:20:07 -0300

    Agrego endpoints de gestion de estudiantes, borrar y modificar un curso, y listar miembros de un curso

--- back/src/routes/curso.routes.js
+++ back/src/routes/curso.routes.js
@@ -2,14 +2,22 @@ const { Router } = require('express')
 const cursoController = require('../controllers/curso.controller')
 const validate = require('../middlewares/validate')
 const cursoScheme = require('../middlewares/schemes/curso.scheme')
-const checkRole = require("../middlewares/checkRole")
-const router = Router();
+const checkRole = require('../middlewares/checkRole')
+const router = Router()
 
+router.post('/', validate(cursoScheme.cursoBase), checkRole.checkRoleDocente, cursoController.crear)
+router.get('/', cursoController.listar)
+router.delete('/', validate(cursoScheme.eliminarCurso), checkRole.checkRoleDocente, cursoController.eliminar)
+router.put('/:id', validate(cursoScheme.cursoBase), checkRole.checkRoleDocente, cursoController.actualizar)
+router.get('/:id', cursoController.ver)
 
-router.post('/', validate(cursoScheme.crearCurso),checkRole.checkRoleDocente,cursoController.crear);
-router.get('/:id',cursoController.ver);
-router.get('/',cursoController.listar)
-router.post('/generar-codigo',checkRole.checkRoleDocente,validate(cursoScheme.generarCodigoVinculacion), cursoController.generarCodigoVinculacion);
-router.post('/vincular-estudiante',checkRole.checkRoleEstudiante,validate(cursoScheme.vincularEstudiante),cursoController.vincularEstudiante);
+router.post('/generar-codigo', validate(cursoScheme.generarCodigoVinculacion), checkRole.checkRoleDocente, cursoController.generarCodigoVinculacion)
+router.post('/vincular-estudiante', validate(cursoScheme.vincularEstudiante), checkRole.checkRoleEstudiante, cursoController.vincularEstudiante)
 
-module.exports = router;
+// Rutas para gestion múltiples estudiantes a un curso
+router.post('/:id/estudiantes', validate(cursoScheme.agregarEstudiantes), checkRole.checkRoleDocente, cursoController.agregarEstudiantes)
+router.delete('/:id/estudiantes', validate(cursoScheme.eliminarEstudiantes), checkRole.checkRoleDocente, cursoController.eliminarEstudiante)
+
+router.get('/:id/miembros', validate(cursoScheme.verMiembrosCurso), checkRole.checkRole('D', 'E'), cursoController.verMiembrosCurso)
+
+module.exports = router

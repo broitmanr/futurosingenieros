3addf091961db10b502d0ae012f43710e3733c67
Leslie Monges <lesliemonges95@gmail.com>
2024-08-31 12:26:11 -0300

    Mejoras en los estilos del front

--- front/fi/src/components/CursosActividades/CursosActividades.jsx
+++ front/fi/src/components/CursosActividades/CursosActividades.jsx
@@ -15,7 +15,7 @@ export const CursosActividades = () => {
             <section className="seccionBanner py-5">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-6 mx-auto border border-5 p-4">
+                        <div className="col-md-6 mx-auto banner-box">
                             <h2 className="nombre-materia">Diseño de sistemas</h2>
                             <p><span className="nombre-comision">S32</span> - <span className="anio-comision">Año 2024</span></p>
                         </div>
@@ -42,13 +42,18 @@ export const CursosActividades = () => {
                                         <li className='aside-item'>
                                             <Link className='aside-link' to="#">Calificaciones</Link>
                                         </li>
-                                        <li className='aside-link d-flex align-items-center'>
+                                        <li className='aside-item d-flex align-items-center'>
+                                            <Link className='aside-link' to="#">
                                             <i className="fa fa-bell" aria-hidden="true"></i>
-                                            <Link className='aside-link' to="#">Notificaciones</Link>
+                                            Notificaciones
+                                            </Link>
                                         </li>
-                                        <li className='aside-link d-flex align-items-center'>
-                                            <i class="fa fa-cogs" aria-hidden="true"></i>
-                                            <Link className='aside-link' to="#">Configuracion</Link>
+
+                                        <li className='aside-item d-flex align-items-center'>
+                                            <Link className='aside-link' to="#">
+                                            <i className="fa fa-cogs" aria-hidden="true"></i>
+                                            Configuracion
+                                            </Link>
                                         </li>
                                     </ul>
                                 </nav>
@@ -56,8 +61,8 @@ export const CursosActividades = () => {
                         </div>
 
                         <div className="col-md-9">
-                            <Button variant="primary" onClick={() => setShow (true)} >
-                            <i class="fa fa-plus-square me-2" aria-hidden="true"></i>
+                            <Button className='btn-primary' onClick={() => setShow (true)} >
+                            <i className="fa fa-plus-square me-2" aria-hidden="true"></i>
                                 Crear Instancia
                             </Button>
                             <h4 className="mt-3">Instancias evaluativas creadas</h4>
@@ -66,31 +71,31 @@ export const CursosActividades = () => {
 
                             <div className="actividad d-flex row border border-2 mt-4 p-2">
                                 <div className="col-md-8 d-flex align-items-center">
-                                <i class="fa-solid fa-tasks me-2"></i>
+                                <i className="fa-solid fa-tasks me-2"></i>
                                     <p className='actividad-nombre m-0'>Planificación del cronograma</p></div>
                                 <div className="col-md-4 d-flex justify-content-end">
-                                    <i class="fa fa-pencil me-2" aria-hidden="true"></i>
-                                    <i class="fa-solid fa-trash text-danger"></i>
+                                    <i className="fa fa-pencil me-2" aria-hidden="true"></i>
+                                    <i className="fa-solid fa-trash text-danger"></i>
                                 </div>
                             </div>
 
                             <div className="actividad d-flex row border border-2 mt-4 p-2">
                                 <div className="col-md-8 d-flex align-items-center">
-                                <i class="fa-solid fa-tasks me-2"></i>
+                                <i className="fa-solid fa-tasks me-2"></i>
                                     <p className='actividad-nombre m-0'>Planificación de costos</p></div>
                                 <div className="col-md-4 d-flex justify-content-end">
-                                    <i class="fa fa-pencil me-2" aria-hidden="true"></i>
-                                    <i class="fa-solid fa-trash text-danger"></i>
+                                    <i className="fa fa-pencil me-2" aria-hidden="true"></i>
+                                    <i className="fa-solid fa-trash text-danger"></i>
                                 </div>
                             </div>
 
                             <div className="actividad d-flex row border border-2 mt-4 p-2">
                                 <div className="col-md-8 d-flex align-items-center">
-                                <i class="fa-solid fa-tasks me-2"></i>
+                                <i className="fa-solid fa-tasks me-2"></i>
                                     <p className='actividad-nombre m-0'>Plan de gestion de riesgos</p></div>
                                 <div className="col-md-4 d-flex justify-content-end">
-                                    <i class="fa fa-pencil me-2" aria-hidden="true"></i>
-                                    <i class="fa-solid fa-trash text-danger"></i>
+                                    <i className="fa fa-pencil me-2" aria-hidden="true"></i>
+                                    <i className="fa-solid fa-trash text-danger"></i>
                                 </div>
                             </div>
 

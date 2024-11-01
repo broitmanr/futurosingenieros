a9e880b917e86b242ad7961b70a9bcbd39b6e04b
Roman <broitmanroman@gmail.com>
2024-11-01 01:50:19 -0300

    add: envio de mails automatico (queda comentado)

--- back/src/mailer/sendEmailWithTemplate.js
+++ back/src/mailer/sendEmailWithTemplate.js
@@ -32,6 +32,68 @@ async function mailRegistro (mail, nombre) {
   })
 }
 
+
+async function mailEntrega (mail, nombre) {
+  const mailOptions = {
+    from: NODEMAILER_FROM, // Dirección del remitente
+    to: mail, // Dirección del destinatario (usuario registrado)
+    subject: '¡Felicitaciones por tu registro!', // Asunto del correo
+    text: `Hola ${nombre}, felicitaciones por registrarte en nuestro sitio.`, // Texto en plano
+    html: `
+        <img src="cid:logo@empresa.com" alt="Logo de la Empresa" style="width: 200px;"/><br><hr>
+        <p>Entrega Pendiente ....</p>`,
+    attachments: [
+      {
+        filename: 'logo.png', // Archivo de imagen del logotipo
+        path: '../back/src/utils/logo.png', // Ruta a la imagen en tu servidor local
+        cid: 'logo@empresa.com' // El Content-ID que se usará en el HTML
+      }
+    ]
+  }
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error al enviar el correo:', error)
+      return 'error'
+    }
+    console.log('Correo enviado:', info.response)
+    return 'success'
+  })
+}
+
+
+async function mailEntrega(mails, mensajePersonalizado) {
+  const mailOptions = {
+    from: NODEMAILER_FROM,
+    to: mails.join(','), // Lista de destinatarios separados por coma
+    subject: 'Recordatorio de entrega pendiente',
+    text: mensajePersonalizado, // Texto en plano
+    html: `
+        <img src="cid:logo@empresa.com" alt="Logo de la Empresa" style="width: 200px;"/><br><hr>
+        <p>${mensajePersonalizado}</p>`,
+    attachments: [
+      {
+        filename: 'logo.png',
+        path: '../back/src/utils/logo.png',
+        cid: 'logo@empresa.com'
+      }
+    ]
+  };
+
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error al enviar el correo:', error);
+        reject(error);
+      } else {
+        console.log('Correo enviado:', info.response);
+        resolve('success');
+      }
+    });
+  });
+}
+
+
 module.exports = {
-  mailRegistro
+  mailRegistro, mailEntrega
 }
--- back/src/services/notificacionService.js
+++ back/src/services/notificacionService.js
@@ -60,97 +60,102 @@ async function crearNotificacionesParaAlumnos(usuarioIds, mensaje, tipoNotificac
 
 
 async function enviarNotificacionesVencimientoEntrega() {
-    console.log(yellow('Creando notificaciones de vencimiento'))
-    // Obtener la fecha actual
+    console.log(yellow('Creando notificaciones de vencimiento'));
+
     const fechaActual = new Date();
 
-    // Establecer la fecha de vencimiento a 3 días a partir de hoy
-    const fechaVencimiento = new Date(fechaActual);
-    fechaVencimiento.setDate(fechaActual.getDate() + 3);
 
-    // Asegúrate de que las fechas se almacenen en UTC
-    const inicioDelDia = new Date(fechaVencimiento);
-    inicioDelDia.setUTCHours(0, 0, 0, 0); // 00:00:00 UTC del día de vencimiento
+    const fechasVencimiento = [
+        { label: '3 días', date: new Date(fechaActual) },
+        { label: '1 día', date: new Date(fechaActual) }
+    ];
 
-    const finDelDia = new Date(fechaVencimiento);
-    finDelDia.setUTCHours(23, 59, 59, 999); // 23:59:59 UTC del día de vencimiento
 
+    fechasVencimiento[0].date.setDate(fechaActual.getDate() + 3);
+    fechasVencimiento[1].date.setDate(fechaActual.getDate() + 1);
 
     try {
-        const entregasProximasAVencer = await models.EntregaPactada.findAll({
-            where: {
-                fechavto1: {
-                    [Op.between]: [inicioDelDia, finDelDia]
-                }
-            },
-            include: [
-                {
-                    model: models.InstanciaEvaluativa,
-                    include: [
-                        {
-                            model: models.Curso,
-                            include: [
-                                {
-                                    model: models.Persona,
-                                    required: true,
-                                    where: { rol: 'A' },
-                                    include: [{ model: models.Usuario }]
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
-        });
-        for (const entrega of entregasProximasAVencer) {
-            const cursoAlumnos = entrega.InstanciaEvaluativa.Curso.Personas;
+        for (const { label, date } of fechasVencimiento) {
+            console.log(label)
+            const inicioDelDia = new Date(date);
+            inicioDelDia.setUTCHours(0, 0, 0, 0); // 00:00:00 UTC del día de vencimiento
 
-            // Obtener las entregas relacionadas a la entrega pactada
-            const entregas = await models.Entrega.findAll({
-                where: { entregaPactada_id: entrega.ID },
-                attributes: ['ID'] // Solo necesitamos los IDs de las entregas
-            });
+            const finDelDia = new Date(date);
+            finDelDia.setUTCHours(23, 59, 59, 999); // 23:59:59 UTC del día de vencimiento
 
-            // Obtener los IDs de las entregas
-            const entregaIds = entregas.map(e => e.ID);
-
-
-
-            // Obtener los alumnos que han hecho entregas
-            const entregasRealizadas = await models.PersonaXEntrega.findAll({
+            const entregasProximasAVencer = await models.EntregaPactada.findAll({
                 where: {
-                    entrega_id: { [Op.in]: entregaIds }
+                    fechavto1: {
+                        [Op.between]: [inicioDelDia, finDelDia]
+                    }
                 },
-                attributes: ['persona_id']
+                include: [
+                    {
+                        model: models.InstanciaEvaluativa,
+                        include: [
+                            {
+                                model: models.Curso,
+                                include: [
+                                    {
+                                        model: models.Persona,
+                                        required: true,
+                                        where: { rol: 'A' },
+                                        include: [{ model: models.Usuario }]
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
             });
 
-            // Extraer solo los IDs de las personas que han realizado entregas
-            const personaIdsConEntrega = new Set(entregasRealizadas.map(e => e.persona_id));
+            for (const entrega of entregasProximasAVencer) {
+                const cursoAlumnos = entrega.InstanciaEvaluativa.Curso.Personas;
 
-            // Filtrar los alumnos que no han realizado entregas
-            const alumnosSinEntrega = cursoAlumnos.filter(alumno =>
-                !personaIdsConEntrega.has(alumno.ID) &&
-                alumno.Usuario // Solo aquellos que tienen un usuario asociado
-            );
-
-            // Si hay alumnos que no han realizado entregas, crear notificaciones
-            if (alumnosSinEntrega.length > 0) {
-                const mensaje = `La entrega pactada "${entrega.nombre}" vence en 3 días. Recuerda completarla a tiempo.`;
-                const tipoNotificacionId = 3; // Cambia según el ID de tipo de notificación
-
-
-                const notificaciones = alumnosSinEntrega.map(alumno => {
-                    return {
-                        detalle: mensaje,
-                        leido: false,
-                        tipoNotificacion_id: tipoNotificacionId,
-                        usuario_id: alumno.Usuario.ID,
-                        updated_by: 'Sistema',
-                        fecha: new Date()
-                    };
+                // Obtener las entregas relacionadas a la entrega pactada
+                const entregas = await models.Entrega.findAll({
+                    where: { entregaPactada_id: entrega.ID },
+                    attributes: ['ID'] // Solo necesitamos los IDs de las entregas
                 });
 
-                await models.Notificacion.bulkCreate(notificaciones);
+                // Obtener los IDs de las entregas
+                const entregaIds = entregas.map(e => e.ID);
+
+                // Obtener los alumnos que han hecho entregas
+                const entregasRealizadas = await models.PersonaXEntrega.findAll({
+                    where: {
+                        entrega_id: { [Op.in]: entregaIds }
+                    },
+                    attributes: ['persona_id']
+                });
+
+                // Extraer solo los IDs de las personas que han realizado entregas
+                const personaIdsConEntrega = new Set(entregasRealizadas.map(e => e.persona_id));
+
+                // Filtrar los alumnos que no han realizado entregas
+                const alumnosSinEntrega = cursoAlumnos.filter(alumno =>
+                    !personaIdsConEntrega.has(alumno.ID) &&
+                    alumno.Usuario // Solo aquellos que tienen un usuario asociado
+                );
+
+                // Si hay alumnos que no han realizado entregas, crear notificaciones
+                if (alumnosSinEntrega.length > 0) {
+                    const mensaje = `La entrega pactada "${entrega.nombre}" vence en ${label}. Recuerda completarla a tiempo.`;
+                    const tipoNotificacionId = 3; // Cambia según el ID de tipo de notificación
+
+                    const notificaciones = alumnosSinEntrega.map(alumno => {
+                        return {
+                            detalle: mensaje,
+                            leido: false,
+                            tipoNotificacion_id: tipoNotificacionId,
+                            usuario_id: alumno.Usuario.ID,
+                            updated_by: 'Sistema',
+                            fecha: new Date()
+                        };
+                    });
+
+                    await models.Notificacion.bulkCreate(notificaciones);
+                }
             }
         }
         console.log('Notificaciones de vencimiento creadas correctamente.');
@@ -159,6 +164,7 @@ async function enviarNotificacionesVencimientoEntrega() {
         throw error;
     }
 }
+
 
 
 module.exports = {

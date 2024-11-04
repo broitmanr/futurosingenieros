fdfb9096dd42b680a336d32a01cad98d4a77d391
Roman <broitmanroman@gmail.com>
2024-11-03 23:26:57 -0300

    feat: agrego mejoras al registro

--- back/src/controllers/auth.controller.js
+++ back/src/controllers/auth.controller.js
@@ -5,6 +5,7 @@ const transporter = require('../mailer/sendEmailWithTemplate')
 const jwt = require('../middlewares/signJWT')
 const rolservice = require('../services/rolservice')
 const isPasswordValid = require('../const/passwordValidation')
+const {Op} = require("sequelize");
 
 module.exports = {
   login: async (req, res, next) => {
@@ -49,7 +50,18 @@ module.exports = {
 
   registrarse: async (req, res, next) => {
     try {
-      // Validar la contraseña
+      const { legajo, password,email,confirmEmail,confirmPassword } = req.body
+
+      if (password !== confirmPassword){
+        return next({...errors.ConflictError,details:'Ambas contraseñas deben coincidir'})
+      }
+
+      if (email !== confirmEmail){
+        return next({...errors.ConflictError,details:'Ambos mails deben coincidir'})
+      }
+
+
+
       if (!isPasswordValid(req.body.password)) {
         return next({
           ...errors.ValidationError,
@@ -57,27 +69,63 @@ module.exports = {
         })
       }
 
-      const persona = await models.Persona.findOne({
-        where: {
-          legajo: req.body.legajo
+      const usuario = await models.Usuario.findOne({
+        where:{
+          mail:email,
         }
       })
-      // Todo: verificar que el mail termine o con frlp.utn.edu.ar
-      req.body.password = bcrypt.hashSync(req.body.password, 10)
-      // const user = await models.Usuario.create({
-      //   mail: req.body.mail,
-      //   password: req.body.password,
-      //   rol: rolservice.rolByMail(req.body.mail) ? 'D' : 'A',
-      //   persona_id: persona ? persona.ID : null
-      // })
 
-      const nombre = persona ? persona.nombre : user.mail.split('@')[0]
+      if (usuario){
+        return next({
+          ...errors.ConflictError,
+          details: 'Ya existe un usuario con ese email'
+        })
+      }
+
+      const persona = await models.Persona.findOne({
+        where: {
+          legajo: legajo
+        },
+        include:{model:models.Usuario, required:false}
+      })
+
+      if (!persona) {
+        res.status(404).json({
+          persona_id: null
+        })
+      }
+
+      if (persona.Usuario){
+        return next({
+          ...errors.ConflictError,
+          details: 'La persona que desea registrar ya tiene un usuario registrado'
+        })
+      }
+
+      // Todo: verificar que el mail termine o con frlp.utn.edu.ar
+      if (!rolservice.isFrlpMail(email)){
+        return next({
+          ...errors.ConflictError,
+          details: 'El mail debe ser emitido por la UTN FRLP'
+        })
+      }
+
+      const passwordHashed= bcrypt.hashSync(password, 10)
+
+      const user = await models.Usuario.create({
+        mail: email,
+        password: passwordHashed,
+        rol: rolservice.rolByMail(email) ? 'D' : 'A',
+        persona_id: persona ? persona.ID : null
+      })
+
+      const name = persona ? persona.nombre : user.mail.split('@')[0]
 
       try {
-        await transporter.mailRegistro(req.body.mail, nombre)
+        // await transporter.mailRegistro(email, name)
       } catch (e) {
-        console.log('Error al enviar mail de registro:', e)
-        return next({ ...errors.InternalServerError, details: e.message })
+
+        return next({ ...errors.InternalServerError, details:'No se pudo enviar mail de registro'})
       }
 
       res.json({
@@ -90,6 +138,102 @@ module.exports = {
       return next(err)
     }
   },
+
+  registrarseDe0: async (req, res, next) => {
+    try {
+      const { legajo, password,email,confirmEmail,confirmPassword } = req.body.usuario
+      const { nombre, apellido,documento} = req.body.persona
+
+
+      if (password !== confirmPassword){
+        return next({...errors.ConflictError,details:'Ambas contraseñas deben coincidir'})
+      }
+
+      if (email !== confirmEmail){
+        return next({...errors.ConflictError,details:'Ambos mails deben coincidir'})
+      }
+
+
+
+      if (!isPasswordValid(req.body.password)) {
+        return next({
+          ...errors.ValidationError,
+          details: 'La contraseña debe tener al menos 8 caracteres alfanuméricos'
+        })
+      }
+
+      const usuario = await models.Usuario.findOne({
+        where:{
+          mail:email,
+        }
+      })
+
+      if (usuario){
+        return next({
+          ...errors.ConflictError,
+          details: 'Ya existe un usuario con ese email'
+        })
+      }
+
+      const persona = await models.Persona.findOne({
+        where: {
+          [Op.or]: [{ legajo: legajo }, { dni: documento }],
+        },
+        include:{model:models.Usuario, required:false}
+      })
+
+      if (persona){
+        return next({
+          ...errors.ConflictError,
+          details: 'Ya existe una persona registrada con esos datos'
+        })
+      }
+
+      const personaNew = await models.Persona.create({
+        nombre: nombre,
+        apellido: apellido,
+        dni: documento,
+        rol: rolservice.rolByMail(req.body.mail) ? 'D' : 'A',
+      })
+
+      // Todo: verificar que el mail termine o con frlp.utn.edu.ar
+      if (!rolservice.isFrlpMail(email)){
+        return next({
+          ...errors.ConflictError,
+          details: 'El mail debe ser emitido por la UTN FRLP'
+        })
+      }
+
+      const passwordHashed= bcrypt.hashSync(password, 10)
+
+      const user = await models.Usuario.create({
+        mail: email,
+        password: passwordHashed,
+        rol: rolservice.rolByMail(email) ? 'D' : 'A',
+        persona_id: personaNew.ID
+      })
+
+      const name = personaNew ? personaNew.nombre : user.mail.split('@')[0]
+
+      try {
+        // await transporter.mailRegistro(email, name)
+      } catch (e) {
+
+        return next({ ...errors.InternalServerError, details:'No se pudo enviar mail de registro'})
+      }
+
+      res.json({
+        success: true,
+        data: {
+          // id: user.ID
+        }
+      })
+    } catch (err) {
+      return next(err)
+    }
+  },
+
+
   logout: async (req, res, next) => {
     res.clearCookie('jwt')
     res.json({ success: true })

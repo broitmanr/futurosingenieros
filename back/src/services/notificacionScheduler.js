371ddada0e39a4e99e111f32ac694a34ee4815ef
Roman <broitmanroman@gmail.com>
2024-11-13 01:25:50 -0300

    feat: envio de mails

--- back/src/services/notificacionScheduler.js
+++ back/src/services/notificacionScheduler.js
@@ -1,19 +1,24 @@
 const cron = require('node-cron');
 const { enviarNotificacionesVencimientoEntrega } = require('./notificacionService');
-const { enviarCorreosVencimientoEntrega } = require('../mailer/mailService');
+const { enviarCorreosVencimientoEntrega, enviarCorreosVencimientoEntregaHoy} = require('../mailer/mailService');
 const {yellow} = require("picocolors");
 
 function iniciarNotificacionesVencimiento() {
     // Configuración para ejecutar la tarea todos los días a las 8:00 AM
-    cron.schedule('0 6 * * *', async () => {
-        console.log(yellow('Ejecutando tarea programada para verificar entregas pactadas'));
-        await enviarNotificacionesVencimientoEntrega();
+    // cron.schedule('0 6 * * *', async () => {
+    //     console.log(yellow('Ejecutando tarea programada para verificar entregas pactadas'));
+    //     await enviarNotificacionesVencimientoEntrega();
+    // });
+
+    cron.schedule('0 10 * * *', async () => {
+        console.log(yellow('Ejecutando tarea programada para enviar mail'));
+        await enviarCorreosVencimientoEntrega();
     });
 
-    // cron.schedule('0 9 * * *', async () => {
-    //     console.log(yellow('Ejecutando tarea programada para enviar mail'));
-    //     await enviarCorreosVencimientoEntrega();
-    // });
+    cron.schedule('0 8 * * *', async () => {
+        console.log(yellow('Ejecutando tarea programada para enviar mail'));
+        await enviarCorreosVencimientoEntregaHoy();
+    });
 }
 
 module.exports = {
